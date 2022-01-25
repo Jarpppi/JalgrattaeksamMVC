96f52ceb25b30b0cbe10ad2bb7437136240723ce
Opilane TTHK <opilane@tthk.ee>
2022-01-25 13:18:34 +0200

    mardo

---
+++ JalgrattaeksamMVC/Data/Migrations/20220125111326_jarrrrpi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JalgrattaeksamMVC.Data.Migrations
+{
+    public partial class jarrrrpi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
